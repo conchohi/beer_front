3bc5749e103eb8260a0abf582b44c05afcf0c9e6
JeYong <pjy3306@naver.com>
2024-06-03 17:48:39 +0900

    mainpage 0603

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: ["react-app", "react-app/jest"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "no-undef": "off",
+    "import/first": "off",
+  },
+  overrides: [
+    {
+      files: ["src/components/VideoRoom/janus.js"],
+      rules: {
+        "no-undef": "off",
+        "react-hooks/rules-of-hooks": "off",
+        "react-hooks/exhaustive-deps": "off",
+      },
+    },
+  ],
+};
