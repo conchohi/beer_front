fdacf0b1bc1b9e0b2410c6e8f3244104a3c3fbaa
yoo yoo <yyaong425@google.com>
2024-06-03 10:21:16 +0900

    lets get started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Header.js';
+import Footer from './Footer.js';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <h1>뭐가 또 문제야</h1>
+      <Footer/>
     </div>
   );
 }
