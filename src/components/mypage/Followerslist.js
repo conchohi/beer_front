3f3cfe1ac455d5eb308c9aae3a0919c57ad6fc4a
YangShin <g56261822@gmail.com>
2024-06-03 14:51:27 +0900

    kys3

--- src/components/mypage/Followerslist.js
+++ src/components/mypage/Followerslist.js
@@ -1,26 +1,32 @@
 import React from "react";
+import { FaUser } from "react-icons/fa"; // 리액트 아이콘 추가
 
 // 홍길동 배열을 컴포넌트 바깥에 선언
 const names = ["홍길동", "김길동", "김민수", "이철수", "박지성", "홍길동"];
 
 function Followerslist() {
   return (
-    <div className="flex w-full">
-      <div className="border px-4 py-2 w-1/2 h-[90vh] text-3xl text-white font-GmarketSans flex flex-col items-center justify-center p-8 bg-gray-800">
-        <div className="text-pink-500 mb-12">팔로잉 목록</div>
-        <div className=" w-full">
+    <div className="flex flex-col lg:flex-row w-full">
+      <div className="w-full lg:w-1/2 h-60 lg:h-80 text-lg lg:text-xl text-white font-GmarketSans flex flex-col items-center justify-center p-4 lg:p-8">
+        <div className="text-pink-500 mb-4 lg:mb-8">팔로잉 목록</div>
+        <hr className="w-full border-pink-500 mb-4 lg:mb-8" />
+        <div className="w-full overflow-x-auto">
           <table className="table-auto w-full text-left text-white">
             <thead>
               <tr>
-                <th className="text-center">이름</th>
-                <th className="w-3/5 text-right cursor-pointer pr-4 md:pr-6 lg:pr-8">기능</th>
+                <th className="pl-2 lg:pl-4 text-left">이름</th>
+                <th className="pl-2 lg:pl-4 text-left">기능</th>
               </tr>
             </thead>
             <tbody>
               {names.map((name, index) => (
                 <tr key={index}>
-                  <td className="text-center">{name}</td>
-                  <td className="w-3/5 text-right text-pink-500 cursor-pointer pr-4 md:pr-6 lg:pr-8">삭제</td>
+                  <td className="pl-2 lg:pl-4 flex items-center">
+                    <FaUser className="mr-2" /> {name}
+                  </td>
+                  <td className="pl-2 lg:pl-4 text-pink-500 cursor-pointer">
+                    삭제
+                  </td>
                 </tr>
               ))}
             </tbody>
@@ -28,26 +34,35 @@ function Followerslist() {
         </div>
       </div>
 
-      <div className="border px-4 py-2 w-1/2 h-[90vh] text-3xl text-white font-GmarketSans flex flex-col items-center justify-center p-8 bg-gray-800">
-        <div className="text-pink-500 mb-12">팔로워 목록</div>
-        <div className=" w-full">
+      <div className="hidden lg:block w-1 border-pink-500 border-l-2 h-80" />
+
+      <div className="w-full lg:w-1/2 h-60 lg:h-80 text-lg lg:text-xl text-white font-GmarketSans flex flex-col items-center justify-center p-4 lg:p-8">
+      <div className="w-full lg:w-1/2 h-60 lg:h-80 text-lg lg:text-xl text-white font-GmarketSans flex flex-col items-center justify-center p-4 lg:p-8">
+        <div className="text-pink-500 mb-4 lg:mb-8">팔로잉 목록</div>
+        <hr className="w-full border-pink-500 mb-4 lg:mb-8" />
+        <div className="w-full overflow-x-auto">
           <table className="table-auto w-full text-left text-white">
             <thead>
               <tr>
-                <th className="text-center">이름</th>
-                <th className="w-3/5 text-right cursor-pointer pr-4 md:pr-6 lg:pr-8">기능</th>
+                <th className="pl-2 lg:pl-4 text-left">이름</th>
+                <th className="pl-2 lg:pl-4 text-left">기능</th>
               </tr>
             </thead>
             <tbody>
               {names.map((name, index) => (
                 <tr key={index}>
-                  <td className="text-center">{name}</td>
-                  <td className="w-3/5 text-right text-pink-500 cursor-pointer pr-4 md:pr-6 lg:pr-8">삭제</td>
+                  <td className="pl-2 lg:pl-4 flex items-center">
+                    <FaUser className="mr-2" /> {name}
+                  </td>
+                  <td className="pl-2 lg:pl-4 text-pink-500 cursor-pointer">
+                    삭제
+                  </td>
                 </tr>
               ))}
             </tbody>
           </table>
         </div>
+      </div>
       </div>
     </div>
   );
--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -10,9 +10,13 @@ const Mypage = () => {
 
   return (
     <BasicLayout>
-      <div className="bg-gray-800 min-h-screen text-white font-GmarketSans p-40 flex flex-col">
-        <div className="bg-gray-700 rounded-2xl flex flex-col justify-between">
-          <div className="bg-gray-800 rounded-2xl p-20 text-left m-6">
+      <div className=" min-h-screen text-white font-GmarketSans p-40 flex flex-col">
+        <div className="border px-4 py-2 rounded-2xl flex flex-col justify-between">
+          <div className=" rounded-2xl p-20 text-left m-6">
+          <p className="m-0 text-[2rem] sm:text-[2rem] bg-gradient-to-r from-[#8f00ff] to-[#f400b0] bg-clip-text text-transparent w-fit">
+      마이 페이지
+    </p>
+           <hr className="my-8 border-gray-600" />
             <ProfilePageInfo handleOpen={handleOpen} />
             <hr className="my-8 border-gray-600" />
             <Followerslist />
