ae1ca85ebebd25d868cbb6562b5df31a16659145
conchohi <lar3242@gmail.com>
2024-06-03 10:37:28 +0900

    인덱스

---
+++ src/pages/IndexPage.js
@@ -0,0 +1,11 @@
+import ChatList from "../component/chatlist/ChatList";
+import Loading from "../component/load/Loading";
+import VideoComponent from "../component/test/RtcTest";
+
+const IndexPage = () => {
+    return ( <>
+        <VideoComponent/>
+    </> );
+}
+ 
+export default IndexPage;
