937e1e24036714ca927d35e598e432ccbc0dbe8a
JeYong <pjy3306@naver.com>
2024-06-05 17:37:35 +0900

    InfoPage

--- src/pages/GameInfo.js
+++ src/pages/GameInfo.js
@@ -73,22 +73,6 @@ function GameInfo() {
           />
         </div>
       </div>
-      <div className="animate-bounce mt-2">
-        <svg
-          className="w-12 h-12 text-white"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M19 9l-7 7-7-7"
-          ></path>
-        </svg>
-      </div>
 
       <GameModal
         isOpen={isModalOpen}
