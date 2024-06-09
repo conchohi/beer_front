af99ebaf7c3d6cceada9946329cd580cc7454c4f
ParkJeYong <154851877+ParkJeYong@users.noreply.github.com>
2024-06-09 23:17:08 +0900

    참여방법 페이지 수정

--- src/pages/GameInfo.js
+++ src/pages/GameInfo.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import GameModal from "../components/Modal/game/GameModal";
+import Card from "../components/Card";
 
 function GameInfo() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -11,75 +12,82 @@ function GameInfo() {
   };
 
   return (
-    <div className="main">
-      <h1 className="text-center font-bold text-3xl mb-4 mt-10">게임 가이드</h1>
-      {/* flex 클래스를 사용하여 아이템들을 가로 정렬합니다. */}
-      <div className="flex justify-center gap-24 p-4">
-        {/* 각 roomMake div를 클릭 가능하게 하고, onClick 이벤트를 추가하여 이미지 클릭 시 모달 창이 열리도록 함 */}
-        <div
-          className="relative flex flex-col items-center justify-center cursor-pointer"
-          onClick={() => openModal("이미지 게임")}
-        >
-          <p>이미지 게임</p>
-          <img
-            className="mb-2"
-            src={`${process.env.PUBLIC_URL}/img/방 개설.png`}
-            alt="이미지 게임"
-          />
-          {/* Tailwind CSS를 사용하여 버튼 스타일 적용 */}
+
+      <div className="font-bold text-gray-200 flex flex-col pt-16 break-keep relative min-h-screen">
+        <div className="text-center py-10">
+          <h1 className="text-4xl font-bold mb-2">
+            게임 가이드{" "}
+            <span role="img" aria-label="game">
+              🎮
+            </span>
+          </h1>
+          <div className="h-1 w-20 bg-yellow-400 mx-auto my-4"></div>
+          <p className="text-gray-500 mb-6">
+            다양한 게임 가이드를 확인하고 즐겨보세요!
+          </p>
+          <h2 className="text-2xl font-bold mb-10">
+            게임 방법을 알아보고 즐겨보세요!
+          </h2>
         </div>
-        <div
-          className="relative flex flex-col items-center justify-center cursor-pointer"
-          onClick={() => openModal("캐치 마인드")}
-        >
-          <p>캐치 마인드</p>
-          <img
-            className="mb-2"
-            src={`${process.env.PUBLIC_URL}/img/방 개설.png`}
-            alt="이용시 주의 사항"
+
+        <div className="flex justify-center items-center gap-8 flex-wrap">
+          <Card
+            color="bg-yellow-300"
+            onClick={() => openModal("이미지 게임")}
+            title="이미지 게임"
+            imageSrc={`${process.env.PUBLIC_URL}/img/방 개설.png`}
           />
-        </div>
-        <div
-          className="relative flex flex-col items-center justify-center cursor-pointer"
-          onClick={() => openModal("고요 속의 외침")}
-        >
-          <p>고요 속의 외침</p>
-          <img
-            className="mb-2"
-            src={`${process.env.PUBLIC_URL}/img/방 개설.png`}
-            alt="게시판 이용 방법"
+          <Card
+            color="bg-yellow-400"
+            onClick={() => openModal("캐치 마인드")}
+            title="캐치 마인드"
+            imageSrc={`${process.env.PUBLIC_URL}/img/방 개설.png`}
           />
-        </div>
-        <div
-          className="relative flex flex-col items-center justify-center cursor-pointer"
-          onClick={() => openModal("인물 퀴즈")}
-        >
-          <p>인물 퀴즈</p>
-          <img
-            className="mb-2"
-            src={`${process.env.PUBLIC_URL}/img/방 개설.png`}
-            alt="게시판 이용 방법"
+          <Card
+            color="bg-yellow-500"
+            onClick={() => openModal("고요 속의 외침")}
+            title="고요 속의 외침"
+            imageSrc={`${process.env.PUBLIC_URL}/img/방 개설.png`}
           />
-        </div>
-        <div
-          className="relative flex flex-col items-center justify-center cursor-pointer"
-          onClick={() => openModal("역전의 한방")}
-        >
-          <p>역전의 한방</p>
-          <img
-            className="mb-2"
-            src={`${process.env.PUBLIC_URL}/img/방 개설.png`}
-            alt="게시판 이용 방법"
+          <Card
+            color="bg-yellow-600"
+            onClick={() => openModal("인물 퀴즈")}
+            title="인물 퀴즈"
+            imageSrc={`${process.env.PUBLIC_URL}/img/방 개설.png`}
+          />
+          <Card
+            color="bg-yellow-700"
+            onClick={() => openModal("역전의 한방")}
+            title="역전의 한방"
+            imageSrc={`${process.env.PUBLIC_URL}/img/방 개설.png`}
           />
         </div>
+
+        {/* 화살표 */}
+        <div className="animate-bounce mt-32">
+          <svg
+            className="mx-auto w-12 h-12 text-white"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M19 9l-7 7-7-7"
+            ></path>
+          </svg>
+        </div>
+
+        <GameModal
+          isOpen={isModalOpen}
+          closeModal={() => setIsModalOpen(false)}
+          title={modalContent}
+        ></GameModal>
       </div>
 
-      <GameModal
-        isOpen={isModalOpen}
-        closeModal={() => setIsModalOpen(false)}
-        title={modalContent}
-      ></GameModal>
-    </div>
   );
 }
 
