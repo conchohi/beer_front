5a646b4464aacba74d8bedc783e03ff7e35fcb38
JeYong <pjy3306@naver.com>
2024-06-10 10:46:47 +0900

    LoginMain.js MovingCat

---
+++ src/components/animation/MovingCat.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useRef } from "react";
+import lottie from "lottie-web";
+import animationData from "./MovingCat.json";
+
+const MovingCat = () => {
+  const animationContainer = useRef(null);
+
+  useEffect(() => {
+    const animation = lottie.loadAnimation({
+      container: animationContainer.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+    });
+
+    return () => animation.destroy(); // Clean up on unmount
+  }, []);
+
+  return (
+    <div ref={animationContainer} style={{ width: "30vw", height: "30vw" }} />
+  );
+};
+
+export default MovingCat;
