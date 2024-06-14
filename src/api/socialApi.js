6ac6f4fed55d1c70bc531dafe7ce23e41a940146
concho <156064008+conchohi@users.noreply.github.com>
2024-06-14 11:44:24 +0900

    Update socialApi.js

--- src/api/socialApi.js
+++ src/api/socialApi.js
@@ -1,9 +1,9 @@
 import { API_SERVER_HOST } from "./axios_intercepter";
 
 export const onNaverLogin = () => {
-    window.location.href = `${API_SERVER_HOST}/api/oauth2/authorization/naver`;
+    window.location.href = `${API_SERVER_HOST}/oauth2/authorization/naver`;
 }
 
 export const onKakaoLogin = () => {
-    window.location.href = `${API_SERVER_HOST}/api/oauth2/authorization/kakao`;
+    window.location.href = `${API_SERVER_HOST}/oauth2/authorization/kakao`;
 }
