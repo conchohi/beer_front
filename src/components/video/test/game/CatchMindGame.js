b696ed6e83c4f654fdb0fcb95125c3694eb3d0a8
conchohi <lar3242@gmail.com>
2024-06-14 12:28:18 +0900

    import 수정

--- src/components/video/test/game/CatchMindGame.js
+++ src/components/video/test/game/CatchMindGame.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { Stomp } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
+import { WEB_SOCKET_SERVER } from '../../../../api/websocketApi';
 
 const CatchMindGame = ({ roomNo, nickname, participantList = [] }) => {
   const canvasRef = useRef(null);
