7717a66fad3a590879dea3dc81fbaeca8149571b
conchohi <lar3242@gmail.com>
2024-06-17 09:29:00 +0900

    마이페이지 수정 처리

--- src/components/mypage/image/ImageEditDisplay.js
+++ src/components/mypage/image/ImageEditDisplay.js
@@ -1,45 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import privateApi from '../../../api/axios_intercepter';
+import privateApi, { API_SERVER_HOST } from '../../../api/axios_intercepter';
 
 const ImageEditDisplay = ({ fileName }) => {
-    const [imageSrc, setImageSrc] = useState(null);
-
-    useEffect(() => {
-        const fetchImage = async () => {
-            try {
-                if (fileName && !fileName.startsWith("blob:")) { // Check if the filename is a blob URL
-                    const response = await privateApi.get(`/api/user/${fileName}`, { responseType: 'arraybuffer' });
-                    const base64 = btoa(
-                        new Uint8Array(response.data).reduce(
-                            (data, byte) => data + String.fromCharCode(byte),
-                            ''
-                        )
-                    );
-                    setImageSrc(`data:image/jpeg;base64,${base64}`);
-                } else {
-                    setImageSrc(fileName); // Directly use the blob URL
-                }
-            } catch (error) {
-                console.error('Error fetching image:', error);
-            }
-        };
-
-        fetchImage();
-    }, [fileName]);
-
     return (
         <div>
-            {imageSrc ? (
-                <img 
-                    src={imageSrc} 
-                    alt="profile" 
+            {(fileName==null || !fileName.startsWith("blob:")) ? (
+                <img
+                    src={`${API_SERVER_HOST}/api/user/${fileName}`}
+                    alt="profile"
                     className='w-36 h-36 rounded-full border-4 border-transparent'
                 />
             ) : (
-                <img 
-                    src="/logo/basic.png" 
-                    alt="default" 
-                    className='w-36 h-36 rounded-full border-4 border-transparent' 
+                <img
+                    src={fileName}
+                    alt="profile"
+                    className='w-36 h-36 rounded-full border-4 border-transparent'
                 />
             )}
         </div>
